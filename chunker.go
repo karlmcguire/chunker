9de401cd0422f81efff9be58a9565c02bc0b7993
Karl McGuire <karl@xba.io>
2020-12-23 11:45:38 -0500

    better readability

--- chunker.go
+++ chunker.go
@@ -28,6 +28,7 @@ import (
 |
 V
 open for object
+	- look for "uid" field
 
 
 	"name": "Alice",
@@ -177,92 +178,206 @@ type Quad struct {
 	ObjectVal interface{}
 }
 
+/*
+(subject:"_:dg.3267536919.1" predicate:"name" object_value:<str_val:"Alice" > )
+(subject:"_:dg.3267536919.2" predicate:"name" object_value:<str_val:"Charlie" > ),
+(subject:"_:dg.3267536919.2" predicate:"married" object_value:<bool_val:false > ),
+(subject:"_:dg.3267536919.1" predicate:"friend" object_id:"1000" ),
+(subject:"1000" predicate:"name" object_value:<str_val:"Bob" > ),
+(subject:"_:dg.3267536919.1" predicate:"friend" object_id:"_:dg.3267536919.2" ),
+*/
+
+type object struct {
+	uid string
+}
+
+type walk struct {
+	objects []*object
+
+	curr       *Quad
+	quads      []*Quad
+	openValue  bool
+	openObject bool
+	openArray  bool
+	openUid    bool
+}
+
+func newWalk() *walk {
+	return &walk{
+		objects: make([]*object, 0),
+		quads:   make([]*Quad, 0),
+		curr:    &Quad{},
+	}
+}
+
+func (w *walk) lookingForPred() bool {
+	return w.openObject && !w.openValue && !w.openArray && !w.openUid
+}
+
+func (w *walk) foundPred(p string) {
+	w.openValue = true
+	w.curr.Predicate = p
+}
+
+func (w *walk) lookingForVal() bool {
+	return w.openValue
+}
+
+func (w *walk) foundValString(v string) {
+	w.openValue = false
+	w.curr.ObjectVal = v
+	w.quads = append(w.quads, w.curr)
+	w.curr = &Quad{}
+}
+
+func (w *walk) foundValInt(v int64) {
+	w.openValue = false
+	w.curr.ObjectVal = v
+	w.quads = append(w.quads, w.curr)
+	w.curr = &Quad{}
+}
+
+func (w *walk) foundValFloat(v float64) {
+	w.openValue = false
+	w.curr.ObjectVal = v
+	w.quads = append(w.quads, w.curr)
+	w.curr = &Quad{}
+}
+
+func (w *walk) foundValBool(v bool) {
+	w.openValue = false
+	w.curr.ObjectVal = v
+	w.quads = append(w.quads, w.curr)
+	w.curr = &Quad{}
+}
+
+func (w *walk) lookingForUid() bool {
+	return w.openObject && !w.openUid
+}
+
+func (w *walk) foundUid() {
+	w.openUid = true
+}
+
+func (w *walk) lookingForUidVal() bool {
+	return w.openObject && w.openUid
+}
+
+func (w *walk) foundUidVal(v string) {
+	w.objects[len(w.objects)-1].uid = v
+	w.openUid = false
+}
+
+func (w *walk) foundEmptyObject() {
+	w.curr = &Quad{}
+}
+
+func (w *walk) String() string {
+	o := ""
+	for i := range w.objects {
+		o += fmt.Sprintf("'%s', ", w.objects[i].uid)
+	}
+	if len(o) == 0 {
+		return ""
+	}
+	return o[:len(o)-2]
+}
+
 func Parse(d []byte) ([]*Quad, error) {
 	tape, err := simdjson.Parse(d, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	quad := &Quad{}
-	buff := make([]*Quad, 0)
 	iter := tape.Iter()
-
-	openValue := false
-	openObject := false
-	openArray := false
+	walk := newWalk()
 
 	for {
 		tag := iter.AdvanceInto()
 
 		switch tag {
-
 		case simdjson.TagString:
-			if openValue {
-				openValue = false
-				quad.ObjectVal, _ = iter.String()
-				buff = append(buff, quad)
-				quad = &Quad{}
-			} else {
-				quad.Predicate, _ = iter.String()
-				next := iter.PeekNextTag()
-				if next == simdjson.TagObjectStart {
-					openObject = true
-				} else if next == simdjson.TagArrayStart {
-					openArray = true
-				} else {
-					openValue = true
-				}
+			k, err := iter.String()
+			if err != nil {
+				panic(err)
+			}
+
+			if walk.lookingForUidVal() {
+				walk.foundUidVal(k)
+
+			} else if walk.lookingForUid() && k == "uid" {
+				walk.foundUid()
+
+			} else if walk.lookingForVal() {
+				walk.foundValString(k)
+
+			} else if walk.lookingForPred() {
+				walk.foundPred(k)
 			}
 
 		case simdjson.TagInteger:
-			if openValue {
-				openValue = false
-				quad.ObjectVal, _ = iter.Int()
-				buff = append(buff, quad)
-				quad = &Quad{}
+			v, err := iter.Int()
+			if err != nil {
+				panic(err)
+			}
+			if walk.lookingForVal() {
+				walk.foundValInt(v)
 			}
 
 		case simdjson.TagUint:
-			if openValue {
-				openValue = false
-				quad.ObjectVal, _ = iter.Uint()
-				buff = append(buff, quad)
-				quad = &Quad{}
+			v, err := iter.Uint()
+			if err != nil {
+				panic(err)
+			}
+			if walk.lookingForVal() {
+				walk.foundValInt(int64(v))
 			}
 
 		case simdjson.TagFloat:
-			if openValue {
-				openValue = false
-				quad.ObjectVal, _ = iter.Float()
-				buff = append(buff, quad)
-				quad = &Quad{}
+			v, err := iter.Float()
+			if err != nil {
+				panic(err)
+			}
+			if walk.lookingForVal() {
+				walk.foundValFloat(v)
 			}
 
 		case simdjson.TagBoolTrue:
 			fallthrough
 		case simdjson.TagBoolFalse:
-			if openValue {
-				openValue = false
-				quad.ObjectVal, _ = iter.Bool()
-				buff = append(buff, quad)
-				quad = &Quad{}
+			v, err := iter.Bool()
+			if err != nil {
+				panic(err)
+			}
+			if walk.lookingForVal() {
+				walk.foundValBool(v)
 			}
 
 		case simdjson.TagObjectStart:
+			next := iter.PeekNextTag()
+			if next == simdjson.TagObjectEnd {
+				walk.foundEmptyObject()
+			} else {
+				walk.openObject = true
+				walk.objects = append(walk.objects, &object{})
+			}
+
 		case simdjson.TagObjectEnd:
-			openObject = false
+			if walk.openObject {
+				walk.openObject = false
+				walk.objects = walk.objects[:len(walk.objects)-1]
+			}
 
 		case simdjson.TagArrayStart:
 		case simdjson.TagArrayEnd:
-			openArray = false
 
 		case simdjson.TagNull:
 		case simdjson.TagRoot:
 		case simdjson.TagEnd:
-			return buff, nil
+			return walk.quads, nil
 		}
 
-		fmt.Println(tag, openValue, openObject, openArray)
+		fmt.Println(tag, walk)
 	}
-	return buff, nil
+	return walk.quads, nil
 }
