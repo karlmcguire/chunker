ac4788a5a8ecffb2dccc495a639fe667fe1b10d3
Karl McGuire <karl@xba.io>
2020-12-23 16:58:06 -0500

    in progress

--- chunker.go
+++ chunker.go
@@ -170,6 +170,183 @@ V
 close for object
 
 
+r
+|
+openValue  = false
+openObject = false -> true
+openArray  = false
+openPred   = false
+
+
+{
+|
+openValue  = false
+openObject = true -> false (previously was true set by root)
+openArray  = false
+openPred   = false -> true (next tag is string)
+
+
+    "name": "Alice",
+	|       |
+	openValue  = false -> true (next tag is scalar)
+	openObject = false
+	openArray  = false
+	openPred   = true -> false (openValue is true)
+	        |
+			openValue  = true -> false (found the value)
+			openObject = false
+			openArray  = false
+			openPred   = false -> true (found the value)
+
+
+	"address": {},
+	|          |
+	openValue  = false
+	openObject = false -> true (next tag is ObjectStart)
+	openArray  = false
+	openPred   = true -> false (openObject is true)
+	           |
+			   openValue  = false
+			   openObject = true -> false (next tag is ObjectClose)
+			   openValue  = false
+			   openPred   = false -> true (found the value)
+
+
+	"friend": [
+	|         |
+	openValue  = false
+	openObject = false
+	openArray  = false -> true (next value is ArrayStart)
+	openPred   = true -> false (openArray is true)
+	          |
+			  openValue  = false
+			  openObject = false
+			  openArray  = true -> false
+			  openPred   = false -> true
+
+
+		{
+		|
+		openValue  = false
+		openObject = false
+		openArray  = false
+		openPred   = true (next value is string)
+
+
+			"name": "Charlie",
+			|       |
+			openValue  = false
+			openObject = false
+			openArray  = false
+			openPred   = false
+					|
+					openValue  = false
+					openObject = false
+					openArray  = false
+					openPred   = false
+
+
+			"married": false,
+			|          |
+			openValue  = false
+			openObject = false
+			openArray  = false
+			openPred   = false
+					   |
+					   openValue  = false
+					   openObject = false
+					   openArray  = false
+					   openPred   = false
+
+
+			"address": {}
+			|          |
+			openValue  = false
+			openObject = false
+			openArray  = false
+			openPred   = false
+					   |
+					   openValue  = false
+					   openObject = false
+					   openArray  = false
+					   openPred   = false
+
+
+		}, {
+		|  |
+		openValue  = false
+		openObject = false
+		openArray  = false
+		openPred   = false
+		   |
+		   openValue  = false
+		   openObject = false
+		   openArray  = false
+		   openPred   = false
+
+
+			"uid": "1000",
+		    |      |
+		    openValue  = false
+		    openObject = false
+		    openArray  = false
+		    openPred   = false
+				   |
+				   openValue  = false
+				   openObject = false
+				   openArray  = false
+				   openPred   = false
+
+
+			"name": "Bob",
+			|       |
+			openValue  = false
+			openObject = false
+			openArray  = false
+			openPred   = false
+			        |
+					openValue  = false
+					openObject = false
+					openArray  = false
+					openPred   = false
+
+
+			"address": {}
+			|          |
+			openValue  = false
+			openObject = false
+			openArray  = false
+			openPred   = false
+			           |
+					   openValue  = false
+					   openObject = false
+					   openArray  = false
+					   openPred   = false
+
+
+		}
+		|
+		openValue  = false
+		openObject = false
+		openArray  = false
+		openPred   = false
+
+
+	]
+	|
+	openValue  = false
+	openObject = false
+	openArray  = false
+	openPred   = false
+
+
+}
+|
+openValue  = false
+openObject = false
+openArray  = false
+openPred   = false
+
 */
 
 type Quad struct {
@@ -179,176 +356,23 @@ type Quad struct {
 	ObjectVal interface{}
 }
 
-/*
-(subject:"_:dg.3267536919.1" predicate:"name" object_value:<str_val:"Alice" > )
-(subject:"_:dg.3267536919.2" predicate:"name" object_value:<str_val:"Charlie" > ),
-(subject:"_:dg.3267536919.2" predicate:"married" object_value:<bool_val:false > ),
-(subject:"_:dg.3267536919.1" predicate:"friend" object_id:"1000" ),
-(subject:"1000" predicate:"name" object_value:<str_val:"Bob" > ),
-(subject:"_:dg.3267536919.1" predicate:"friend" object_id:"_:dg.3267536919.2" ),
-*/
-
-var counter = 0
-
-func getNextBlank() string {
-	counter++
-	return fmt.Sprintf("c.%d", counter)
-}
-
-type levelOf uint8
-
-const (
-	OBJECT levelOf = iota
-	ARRAY
-)
-
-type level struct {
-	of    levelOf
-	id    string
-	uid   string
-	quads uint64
-}
-
 type walk struct {
-	curr    *Quad
-	quads   []*Quad
-	waiting []*Quad
-	levels  []*level
-
+	curr       *Quad
+	quads      []*Quad
+	waits      []*Quad
 	openValue  bool
 	openObject bool
 	openArray  bool
-	openUid    bool
+	openPred   bool
 	skip       bool
 }
 
 func newWalk() *walk {
 	return &walk{
-		curr:    &Quad{},
-		quads:   make([]*Quad, 0),
-		waiting: make([]*Quad, 0),
-		levels:  make([]*level, 0),
+		curr:  &Quad{},
+		quads: make([]*Quad, 0),
+		waits: make([]*Quad, 0),
 	}
-}
-
-func (w *walk) getLevel() *level {
-	if len(w.levels) >= 1 {
-		return w.levels[len(w.levels)-1]
-	}
-	return nil
-}
-
-func (w *walk) getLevelUp() *level {
-	if len(w.levels) >= 2 {
-		return w.levels[len(w.levels)-2]
-	}
-	return nil
-}
-
-func (w *walk) lookingForPred() bool {
-	return w.openObject && !w.openValue && !w.openUid
-}
-
-func (w *walk) foundPred(p string, t simdjson.Tag) {
-	if w.curr.Predicate != "" {
-		fmt.Println("replacing ", w.curr.Predicate, " with ", p)
-	}
-	w.curr.Predicate = p
-	if t != simdjson.TagObjectStart && t != simdjson.TagArrayStart {
-		w.openValue = true
-	} else {
-		w.waiting = append(w.waiting, w.curr)
-	}
-}
-
-func (w *walk) lookingForVal() bool {
-	return w.openValue
-}
-
-func (w *walk) foundVal() {
-	w.openValue = false
-
-	// increase the count of the current level
-	level := w.getLevel()
-	if level != nil {
-		level.quads++
-	}
-
-	// if we're within an array, we also want to increase the total count there
-	levelUp := w.getLevelUp()
-	if levelUp != nil && levelUp.of == ARRAY {
-		levelUp.quads++
-	}
-
-	if level.uid != "" {
-		w.curr.Subject = level.uid
-	}
-	w.quads = append(w.quads, w.curr)
-	w.curr = &Quad{}
-}
-
-func (w *walk) foundValString(v string) {
-	w.curr.ObjectVal = v
-	w.foundVal()
-}
-
-func (w *walk) foundValInt(v int64) {
-	w.curr.ObjectVal = v
-	w.foundVal()
-}
-
-func (w *walk) foundValFloat(v float64) {
-	w.curr.ObjectVal = v
-	w.foundVal()
-}
-
-func (w *walk) foundValBool(v bool) {
-	w.curr.ObjectVal = v
-	w.foundVal()
-}
-
-func (w *walk) lookingForUid() bool {
-	return w.openObject && !w.openUid
-}
-
-func (w *walk) foundUid() {
-	w.openUid = true
-}
-
-func (w *walk) lookingForUidVal() bool {
-	return w.openObject && w.openUid
-}
-
-func (w *walk) foundUidVal(v string) {
-	w.getLevel().uid = v
-	w.openUid = false
-}
-
-func (w *walk) foundEmptyObject() {
-	w.skip = true
-	w.openValue = false
-
-	if len(w.waiting) > 0 {
-		if w.waiting[len(w.waiting)-1] == w.curr {
-			fmt.Println("deleting ", w.curr.Predicate)
-			w.waiting = w.waiting[:len(w.waiting)-1]
-		}
-	}
-
-	w.curr = &Quad{}
-}
-
-func (w *walk) foundEmptyArray() {
-	w.skip = true
-	w.openValue = false
-
-	if len(w.waiting) > 0 {
-		if w.waiting[len(w.waiting)-1] == w.curr {
-			w.waiting = w.waiting[:len(w.waiting)-1]
-		}
-	}
-
-	w.curr = &Quad{}
 }
 
 func Parse(d []byte) ([]*Quad, error) {
@@ -361,119 +385,85 @@ func Parse(d []byte) ([]*Quad, error) {
 	walk := newWalk()
 
 	for {
-		tag := iter.AdvanceInto()
+		t, n := iter.AdvanceInto(), iter.PeekNextTag()
 
 		if walk.skip {
 			walk.skip = false
 			continue
 		}
 
-		switch tag {
+		switch n {
 		case simdjson.TagString:
-			k, err := iter.String()
-			if err != nil {
-				panic(err)
-			}
-			if walk.lookingForUidVal() {
-				walk.foundUidVal(k)
-			} else if walk.lookingForUid() && k == "uid" {
-				walk.foundUid()
-			} else if walk.lookingForVal() {
-				walk.foundValString(k)
-			} else if walk.lookingForPred() {
-				walk.foundPred(k, iter.PeekNextTag())
-			}
-
+			fallthrough
 		case simdjson.TagInteger:
-			v, err := iter.Int()
-			if err != nil {
-				panic(err)
-			}
-			if walk.lookingForVal() {
-				walk.foundValInt(v)
-			}
-
+			fallthrough
 		case simdjson.TagUint:
-			v, err := iter.Uint()
-			if err != nil {
-				panic(err)
-			}
-			if walk.lookingForVal() {
-				walk.foundValInt(int64(v))
-			}
-
+			fallthrough
 		case simdjson.TagFloat:
-			v, err := iter.Float()
-			if err != nil {
-				panic(err)
-			}
-			if walk.lookingForVal() {
-				walk.foundValFloat(v)
-			}
-
+			fallthrough
 		case simdjson.TagBoolTrue:
 			fallthrough
 		case simdjson.TagBoolFalse:
-			v, err := iter.Bool()
-			if err != nil {
-				panic(err)
-			}
-			if walk.lookingForVal() {
-				walk.foundValBool(v)
+			if walk.openPred {
+				walk.openPred = false
+				walk.openValue = true
+			} else {
+				walk.openPred = true
+				walk.openValue = false
 			}
 
 		case simdjson.TagObjectStart:
-			next := iter.PeekNextTag()
-			if next == simdjson.TagObjectEnd {
-				walk.foundEmptyObject()
-			} else {
-				walk.openObject = true
-				walk.levels = append(walk.levels, &level{
-					of: OBJECT,
-					id: getNextBlank(),
-				})
-				if walk.lookingForVal() {
-					walk.waiting = append(walk.waiting, walk.curr)
-				}
-			}
-
-		case simdjson.TagObjectEnd:
-			if walk.openObject {
-				walk.openObject = false
-				level := walk.getLevel()
-				if level.uid == "" {
-					for i := uint64(0); i < level.quads; i++ {
-						walk.quads[uint64(len(walk.quads))-1-i].Subject = level.id
-					}
-				} else {
-					// TODO: replace ObjectId for predicates referencing the
-					//       current object
-					/*
-						if len(walk.waiting) > 0 {
-							walk.waiting[len(walk.waiting)-1].ObjectId = level.uid
-							walk.waiting = walk.waiting[:len(walk.waiting)-1]
-						}
-					*/
-				}
-				walk.levels = walk.levels[:len(walk.levels)-1]
+			walk.openObject = true
+			if walk.openPred {
+				walk.openPred = false
 			}
 
 		case simdjson.TagArrayStart:
-			next := iter.PeekNextTag()
-			if next == simdjson.TagArrayEnd {
-				walk.foundEmptyArray()
+			walk.openArray = true
+			if walk.openPred {
+				walk.openPred = false
+			}
+		}
+
+		switch t {
+		case simdjson.TagString:
+			if walk.openPred {
+				walk.openPred = false
+				walk.curr.Predicate, _ = iter.String()
+			} else if walk.openValue {
+				walk.openValue = false
+				walk.curr.ObjectVal, _ = iter.String()
+				walk.quads = append(walk.quads, walk.curr)
+				walk.curr = &Quad{}
+			}
+		case simdjson.TagInteger:
+		case simdjson.TagUint:
+		case simdjson.TagFloat:
+		case simdjson.TagBoolTrue:
+		case simdjson.TagBoolFalse:
+
+		case simdjson.TagObjectStart:
+			walk.openObject = false
+			if n == simdjson.TagObjectEnd {
+				walk.skip = true
 			} else {
-				walk.openArray = true
-				walk.levels = append(walk.levels, &level{
-					of: ARRAY,
-					// don't generate a new id, those are only for objects
-				})
+
+			}
+
+		case simdjson.TagObjectEnd:
+
+		case simdjson.TagArrayStart:
+			walk.openArray = false
+			if n == simdjson.TagArrayEnd {
+				walk.skip = true
+			} else {
+				if walk.openValue {
+					walk.waits = append(walk.waits, walk.curr)
+					walk.curr = &Quad{}
+				}
 			}
 
 		case simdjson.TagArrayEnd:
-			if walk.openArray {
-				walk.openArray = false
-			}
 
 		case simdjson.TagNull:
 		case simdjson.TagRoot:
@@ -481,11 +471,13 @@ func Parse(d []byte) ([]*Quad, error) {
 			return walk.quads, nil
 		}
 
-		fmt.Println(tag, walk.openValue, walk.openObject, walk.openArray)
-		spew.Dump(walk.waiting)
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
+		fmt.Println(t, n)
+		fmt.Printf("openValue  = %v\nopenObject = %v\nopenArray  = %v\nopenPred   = %v\nskip       = %v\n\n",
+			walk.openValue, walk.openObject, walk.openArray, walk.openPred, walk.skip)
+
 	}
+
+	spew.Dump(walk.waits)
+
 	return walk.quads, nil
 }
