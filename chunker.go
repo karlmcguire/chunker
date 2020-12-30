a5a093b901ef0e5fa9be639a6daca8bdd64c65e2
Karl McGuire <karl@xba.io>
2020-12-30 15:57:38 -0500

    more cleaning

--- chunker.go
+++ chunker.go
@@ -14,6 +14,7 @@ const (
 	SCALAR
 	OBJECT
 	ARRAY
+	ARRAY_SCALAR
 )
 
 func (s ParserState) String() string {
@@ -28,20 +29,53 @@ func (s ParserState) String() string {
 		return "OBJECT"
 	case ARRAY:
 		return "ARRAY"
+	case ARRAY_SCALAR:
+		return "ARRAY_SCALAR"
 	}
 	return "?"
+}
+
+type (
+	Depth struct {
+		Levels []*Level
+	}
+
+	Level struct {
+		Type ParserState
+		Uids []string
+	}
+)
+
+func NewDepth() *Depth {
+	return &Depth{
+		Levels: make([]*Level, 0),
+	}
+}
+
+func (d *Depth) Increase(t ParserState) {
+	d.Levels = append(d.Levels, &Level{
+		Type: t,
+		Uids: make([]string, 0),
+	})
+}
+
+func (d *Depth) Decrease(t ParserState) {
+	d.Levels = d.Levels[:len(d.Levels)-1]
 }
 
 type Parser struct {
 	State ParserState
 	Quads []*Quad
+	Depth *Depth
 	Quad  *Quad
+	Skip  bool
 }
 
 func NewParser() *Parser {
 	return &Parser{
 		State: NONE,
 		Quads: make([]*Quad, 0),
+		Depth: NewDepth(),
 		Quad:  &Quad{},
 	}
 }
@@ -63,16 +97,20 @@ func (p *Parser) Parse(iter json.Iter) ([]*Quad, error) {
 // The Parser will continue reading from the tape and calling Scan until it
 // returns true or an error.
 //
-// NOTE: we should only mutate p.State from within this function
+// NOTE: only mutate p.State from within this function
 func (p *Parser) Scan(c, n json.Tag, i json.Iter) (done bool, err error) {
+	if p.Skip {
+		p.Skip = false
+		return
+	}
+
 	defer p.Log(c, n)
 	switch c {
 
 	case json.TagString:
 		switch p.State {
 		case PREDICATE:
-			p.Quad.Predicate, err = i.String()
-			if err != nil {
+			if err = p.FoundPredicate(i.String()); err != nil {
 				return
 			}
 			switch n {
@@ -85,40 +123,96 @@ func (p *Parser) Scan(c, n json.Tag, i json.Iter) (done bool, err error) {
 		case ARRAY:
 		}
 
-	case json.TagInteger:
-		if p.State == SCALAR {
-			if p.State, err = p.Push(i.Int()); err != nil {
-				return
-			}
-		}
-
-	case json.TagUint:
-		if p.State == SCALAR {
-			if p.State, err = p.Push(i.Int()); err != nil {
-				return
-			}
-		}
-
 	case json.TagFloat:
-		if p.State == SCALAR {
-			if p.State, err = p.Push(i.Float()); err != nil {
+		switch p.State {
+		case SCALAR:
+			if p.State, err = p.FoundValue(i.Float()); err != nil {
 				return
 			}
 		}
 
-	case json.TagBoolFalse:
-		fallthrough
-	case json.TagBoolTrue:
-		if p.State == SCALAR {
-			if p.State, err = p.Push(i.Bool()); err != nil {
+	case json.TagUint, json.TagInteger:
+		switch p.State {
+		case SCALAR:
+			if p.State, err = p.FoundValue(i.Int()); err != nil {
+				return
+			}
+		}
+
+	case json.TagBoolFalse, json.TagBoolTrue:
+		switch p.State {
+		case SCALAR:
+			if p.State, err = p.FoundValue(i.Bool()); err != nil {
 				return
 			}
 		}
 
 	case json.TagObjectStart:
+		if n != json.TagObjectEnd {
+			p.Depth.Increase(OBJECT)
+		}
+		switch n {
+		case json.TagString:
+			p.State = PREDICATE
+		case json.TagObjectStart:
+			p.State = OBJECT
+		case json.TagObjectEnd:
+			p.State = PREDICATE
+			p.Skip = true
+		case json.TagArrayStart:
+			p.State = ARRAY
+		}
+
 	case json.TagObjectEnd:
+		p.Depth.Decrease(OBJECT)
+		switch n {
+		case json.TagString:
+			fallthrough
+		case json.TagObjectEnd:
+			fallthrough
+		case json.TagArrayEnd:
+			p.State = PREDICATE
+		case json.TagObjectStart:
+			p.State = OBJECT
+		}
+
 	case json.TagArrayStart:
+		if n != json.TagArrayEnd {
+			p.Depth.Increase(ARRAY)
+		}
+		switch n {
+		case json.TagString:
+			fallthrough
+		case json.TagFloat:
+			fallthrough
+		case json.TagUint, json.TagInteger:
+			fallthrough
+		case json.TagBoolFalse, json.TagBoolTrue:
+			p.State = ARRAY_SCALAR
+		case json.TagObjectStart:
+			p.State = OBJECT
+		case json.TagArrayStart:
+			p.State = ARRAY
+		case json.TagArrayEnd:
+			p.State = PREDICATE
+			p.Skip = true
+		}
+
 	case json.TagArrayEnd:
+		p.Depth.Decrease(ARRAY)
+		switch n {
+		case json.TagString:
+			fallthrough
+		case json.TagObjectEnd:
+			fallthrough
+		case json.TagArrayEnd:
+			p.State = PREDICATE
+		case json.TagArrayStart:
+			p.State = ARRAY
+		case json.TagObjectStart:
+			p.State = OBJECT
+		}
+
 	case json.TagNull:
 	case json.TagRoot:
 	case json.TagEnd:
@@ -127,13 +221,17 @@ func (p *Parser) Scan(c, n json.Tag, i json.Iter) (done bool, err error) {
 	return
 }
 
-// TODO
-func (p *Parser) Push(v interface{}, err error) (ParserState, error) {
+func (p *Parser) FoundPredicate(s string, err error) error {
+	p.Quad.Predicate = s
+	return err
+}
+
+func (p *Parser) FoundValue(v interface{}, err error) (ParserState, error) {
 	return NONE, nil
 }
 
 func (p *Parser) Log(c, n json.Tag) {
-	fmt.Println(c, n, p.State)
+	fmt.Println(c, n, p.State, p.Depth.Levels)
 }
 
 func Parse(d []byte) ([]*Quad, error) {
