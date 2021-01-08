4ce6342ae357ca11e0419d392b6d8e4e11a0b7d1
Karl McGuire <karl@xba.io>
2021-01-08 18:57:34 -0500

    strings

--- chunker.go
+++ chunker.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"runtime"
+	"strings"
 
 	json "github.com/minio/simdjson-go"
 )
@@ -20,14 +21,17 @@ type Quad struct {
 type (
 	ParserState func() (ParserState, error)
 	Parser      struct {
-		Quads  []*Quad
-		Parsed *json.ParsedJson
+		Cursor       uint64
+		StringCursor uint64
+		Quads        []*Quad
+		Parsed       *json.ParsedJson
 	}
 )
 
 func NewParser() *Parser {
 	return &Parser{
-		Quads: make([]*Quad, 0),
+		Cursor: 1,
+		Quads:  make([]*Quad, 0),
 	}
 }
 
@@ -35,7 +39,7 @@ func (p *Parser) Run(d []byte) (err error) {
 	if p.Parsed, err = json.Parse(d, nil); err != nil {
 		return
 	}
-	for state := p.Root; state != nil; {
+	for state := p.Root; state != nil; p.Cursor++ {
 		p.Log(state)
 		if state, err = state(); err != nil {
 			return
@@ -45,12 +49,42 @@ func (p *Parser) Run(d []byte) (err error) {
 }
 
 func (p *Parser) Log(state ParserState) {
-	name := runtime.FuncForPC(reflect.ValueOf(state).Pointer()).Name()
-	fmt.Println(name[len(name)-7 : len(name)-3])
+	line := runtime.FuncForPC(reflect.ValueOf(state).Pointer()).Name()
+	name := strings.Split(line, ".")
+	fmt.Printf("-> %s\n", name[3][:len(name[3])-3])
+}
+
+func (p *Parser) String() string {
+	p.Cursor++
+	length := p.Parsed.Tape[p.Cursor]
+	s := p.Parsed.Strings[p.StringCursor : p.StringCursor+length]
+	p.StringCursor += length
+	return string(s)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 func (p *Parser) Root() (ParserState, error) {
+	n := p.Parsed.Tape[p.Cursor]
+
+	switch byte(n >> 56) {
+	case '{':
+		return p.Object, nil
+	case '[':
+	}
+
 	return nil, nil
+}
+
+func (p *Parser) Object() (ParserState, error) {
+	n := p.Parsed.Tape[p.Cursor]
+
+	switch byte(n >> 56) {
+	case '"':
+		fmt.Println(p.String())
+	default:
+		return nil, nil
+	}
+
+	return p.Object, nil
 }
